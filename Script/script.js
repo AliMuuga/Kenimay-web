92a2c86c75975e49e52529ca1a2203a5faa0fe9b
AliMuuga <alithareaper@gmail.com>
2025-10-01 15:55:03 +0200

    Lets see

--- Script/script.js
+++ Script/script.js
@@ -46,10 +46,3 @@ form.addEventListener('submit', function(e){
       console.error('EmailJS error:', err);
     });
 });
-  window.addEventListener("scroll", function () {
-    const header = document.querySelector(".header");
-    if (window.scrollY > 50) {
-      header.classList.add("shrink");
-    } else {
-      header.classList.remove("shrink");
-    }
